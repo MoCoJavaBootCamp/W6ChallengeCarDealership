34f7c06cdd1f7b91c9f1ec49e0436371c4e80039
azdoolittle <drewdoolittle@gmail.com>
2020-05-29 16:12:29 -0400

    update delete mapping

--- src/main/java/com/example/demo/HomeCtrl.java
+++ src/main/java/com/example/demo/HomeCtrl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,6 +27,19 @@ public class HomeCtrl {
         return "newcategory";
     }
 
+    @RequestMapping("/updatecategory/{id}")
+    public String updateCategory(@PathVariable("id") long id, Model model) {
+        model.addAttribute("category", categoryRepository.findById(id).get());
+        return "newcategory";
+    }
+
+    @RequestMapping("/deletecategory/{id}")
+    public String deleteCategory(@PathVariable("id") long id) {
+        Category category = categoryRepository.findById(id).get();
+        categoryRepository.delete(category);
+        return "redirect:/categorieslist";
+    }
+
     @RequestMapping("/processcategory")
     public String processcategory(@ModelAttribute("category") Category category) {
         categoryRepository.save(category);
@@ -43,6 +57,19 @@ public class HomeCtrl {
         model.addAttribute("car", new Car());
         model.addAttribute("allcategories", categoryRepository.findAll());
         return "newcar";
+    }
+
+    @RequestMapping("/updatecar/{id}")
+    public String updateCar(@PathVariable("id") long id, Model model) {
+        model.addAttribute("car", carRepository.findById(id).get());
+        return "newcar";
+    }
+
+    @RequestMapping("/deletecar/{id}")
+    public String deleteCare(@PathVariable("id") long id) {
+        Car car = carRepository.findById(id).get();
+        carRepository.delete(car);
+        return "redirect:/carslist";
     }
 
     @RequestMapping("/processcar")
